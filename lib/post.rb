91a2b20cebcdc1b41db0a1b76b34186441ff82c1
Nate Miller <natekmiller96@gmail.com>
2018-07-02 22:14:57 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -6,4 +6,13 @@ class Post
     @author = author
     @@all << self
   end
+  def self.all
+    return @@all
+  end
+  def author_name
+    if self.author != nil
+      return self.author.name
+    else
+      return nil
+    end
 end
