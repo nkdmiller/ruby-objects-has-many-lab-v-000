860569e88dd1004d2d092f3adb46e2e7522b8eed
Nate Miller <natekmiller96@gmail.com>
2018-07-02 22:02:17 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,21 @@
 class Artist
+  @@all = []
   attr_accessor :name
   def initialize(name)
     @name = name
     @songs = []
+  end
+  def add_song(song)
+    @songs << song
+    @@all << song
+  end
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+    @songs << song
+    @@all << song
+  end
+  def song_count
+    return @@all.length
   end
 end
