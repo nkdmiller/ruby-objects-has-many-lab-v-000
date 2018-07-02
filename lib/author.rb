4dd5002f7ad053ae7b9256d96d4eccdd359d8392
Nate Miller <natekmiller96@gmail.com>
2018-07-02 22:12:23 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,22 @@
 class Author
+  @@all = []
   attr_accessor :name
-  def initialize
+  def initialize(name)
     @name = name
+    @post = []
   end
+  def posts
+    return @posts
+  end
+  def add_post(post)
+    @post << post
+    post.author = self
+    @@all << song
+  end
+  def add_post_by_title(name)
+    post = Post.new(name)
+    @post << post
+    post.author = self
+    @@all << song 
+  end  
 end
